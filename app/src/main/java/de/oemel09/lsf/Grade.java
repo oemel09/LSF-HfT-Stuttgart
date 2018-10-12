670a1438003d148bc18e20bd04348b615d96077b
oemel09 <oemel09@googlemail.com>
2018-10-12 08:52:47 -0500

    removed lombok

--- app/src/main/java/de/oemel09/lsf/Grade.java
+++ app/src/main/java/de/oemel09/lsf/Grade.java
@@ -1,12 +1,70 @@
 package de.oemel09.lsf;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 class Grade {
 
     private String gradeNr, gradeName, semester, grade, state, ects, attempt, date;
 
+    String getGradeNr() {
+        return gradeNr;
+    }
+
+    void setGradeNr(String gradeNr) {
+        this.gradeNr = gradeNr;
+    }
+
+    String getGradeName() {
+        return gradeName;
+    }
+
+    void setGradeName(String gradeName) {
+        this.gradeName = gradeName;
+    }
+
+    String getSemester() {
+        return semester;
+    }
+
+    void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    String getGrade() {
+        return grade;
+    }
+
+    void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    String getState() {
+        return state;
+    }
+
+    void setState(String state) {
+        this.state = state;
+    }
+
+    String getEcts() {
+        return ects;
+    }
+
+    void setEcts(String ects) {
+        this.ects = ects;
+    }
+
+    String getAttempt() {
+        return attempt;
+    }
+
+    void setAttempt(String attempt) {
+        this.attempt = attempt;
+    }
+
+    String getDate() {
+        return date;
+    }
+
+    void setDate(String date) {
+        this.date = date;
+    }
 }
